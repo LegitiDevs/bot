fb88a38a548ca24441c320f0d2874945f8fa2b78
Omri H <omri.hermon@gmail.com>
2024-12-01 19:23:29 +0200

    try catch my beloved

--- src/client/java/me/omrih/legitimooseBot/client/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/client/java/me/omrih/legitimooseBot/client/mixin/ClientPlayNetworkHandlerMixin.java
@@ -9,22 +9,19 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.io.IOException;
-
 import static me.omrih.legitimooseBot.client.LegitimooseBotClient.CONFIG;
 
 @Mixin(ClientPlayNetworkHandler.class)
 public class ClientPlayNetworkHandlerMixin {
     @Inject(method = "onChatMessage", at = @At("HEAD"))
-    public void messageListener(ChatMessageS2CPacket packet, CallbackInfo ci) throws IOException {
-        if (CONFIG.webhookUrl() == null) {
-        } else {
+    public void messageListener(ChatMessageS2CPacket packet, CallbackInfo ci) {
+        try {
             DiscordWebhook webhook = new DiscordWebhook(CONFIG.webhookUrl());
             webhook.setUsername(packet.sender().toString());
-            EmbedObject embed = new EmbedObject()
-                    .setDescription(packet.body().content());
+            EmbedObject embed = new EmbedObject().setDescription(packet.body().content());
             webhook.getEmbeds().add(embed);
             webhook.execute();
+        } catch (Exception ignored) {
         }
     }
 }
