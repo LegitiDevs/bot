d5aab18e7ba616c2bd695e91b708f3a9fe7a9241
Omri H <omri.hermon@gmail.com>
2024-12-02 13:57:32 +0200

    Player username/head and regex

--- src/client/java/me/omrih/legitimooseBot/client/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/client/java/me/omrih/legitimooseBot/client/mixin/ClientPlayNetworkHandlerMixin.java
@@ -9,6 +9,9 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import static me.omrih.legitimooseBot.client.LegitimooseBotClient.CONFIG;
 
 @Mixin(ClientPlayNetworkHandler.class)
@@ -17,8 +20,20 @@ public class ClientPlayNetworkHandlerMixin {
     public void messageListener(GameMessageS2CPacket packet, CallbackInfo ci) {
         new Thread(() -> {
             try {
+                String message = packet.content().getString();
+                Pattern pattern = Pattern.compile("^([^:]+):");
+                Matcher matcher = pattern.matcher(message);
+
+                String username = "";
+                String cleanMessage = message;
+                if (matcher.find()) {
+                    username = matcher.group(1);
+                    cleanMessage = message.substring(matcher.end()).trim();
+                }
                 DiscordWebhook webhook = new DiscordWebhook(CONFIG.webhookUrl());
-                EmbedObject embed = new EmbedObject().setDescription(packet.content().getString());
+                webhook.setUsername(username);
+                webhook.setAvatarUrl("https://mc-heads.net/avatar/" + username);
+                EmbedObject embed = new EmbedObject().setDescription(cleanMessage);
                 webhook.getEmbeds().add(embed);
                 webhook.execute();
             } catch (Exception ignored) {
