0a4bf80679efa0088e994532db7ee0c967bbc656
PilkeySEK <156579268+PilkeySEK@users.noreply.github.com>
2024-12-02 18:36:56 +0100

    Do stuff

--- src/client/java/me/omrih/legitimooseBot/client/LegitimooseBotClient.java
+++ src/client/java/me/omrih/legitimooseBot/client/LegitimooseBotClient.java
@@ -2,19 +2,28 @@ package me.omrih.legitimooseBot.client;
 
 import me.micartey.webhookly.DiscordWebhook;
 import me.micartey.webhookly.embeds.EmbedObject;
+import me.omrih.legitimooseBot.client.command.ScrapeCommand;
 import me.omrih.legitimooseBot.client.config.LegitimooseBotConfig;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 
 import java.awt.*;
+import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class LegitimooseBotClient implements ClientModInitializer {
     public static final LegitimooseBotConfig CONFIG = LegitimooseBotConfig.createAndLoad();
+    public static final Logger LOGGER = Logger.getLogger("Legitimoose-Bot");
 
     @Override
     public void onInitializeClient() {
+        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+            ScrapeCommand.registerCommand(dispatcher);
+        });
+
         ClientReceiveMessageEvents.GAME.register((message, overlay) -> {
             final Pattern JOIN_PATTERN = Pattern.compile("^\\[\\+]\\s*(?:[^|]+\\|\\s*)?(\\S+)");
             final Pattern CHAT_PATTERN = Pattern.compile("^(?:[^|]+\\|\\s*)?([^:]+):");
---
+++ src/client/java/me/omrih/legitimooseBot/client/Scraper.java
@@ -0,0 +1,54 @@
+package me.omrih.legitimooseBot.client;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.slot.SlotActionType;
+
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+import static me.omrih.legitimooseBot.client.LegitimooseBotClient.LOGGER;
+
+public class Scraper {
+    /*
+    Scrape all worlds (will only scrape world name, description, votes and owner)
+     */
+    public static void scrapeAll() {
+        MinecraftClient client = MinecraftClient.getInstance();
+        assert client.player != null;
+        client.player.networkHandler.sendChatCommand("worlds");
+
+        new Thread(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            assert MinecraftClient.getInstance().interactionManager != null;
+            ScreenHandler currentScreenHandler = client.player.currentScreenHandler;
+            int syncId = currentScreenHandler.syncId;
+            Inventory inv = currentScreenHandler.getSlot(0).inventory;
+            List<Text> tooltip = new ArrayList<>();
+            Item item = Registries.ITEM.get(Identifier.ofVanilla("dirt"));
+            item.appendTooltip(item.getDefaultStack(), Item.TooltipContext.DEFAULT, tooltip, TooltipType.BASIC);
+            try {
+                LOGGER.info(inv.getStack(0).getItem().appendTooltip());
+            } catch (Exception e) {
+                LOGGER.warning(e.getMessage());
+            }
+            MinecraftClient.getInstance().interactionManager.clickSlot(syncId, 0, 0, SlotActionType.PICKUP, client.player);
+        }).start();
+    }
+
+    // inner class
+    public static class ScrapedWorld {
+        String name;
+        String description;
+        String ownerName;
+        boolean resourcePackPresent;
+        String uuidString;
+        int votes;
+    }
+}
