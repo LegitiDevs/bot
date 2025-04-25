570ca34224e30b30463eed15ade4d814aa8daac4
Omri H <76548146+OmriPH@users.noreply.github.com>
2025-04-25 15:12:48 +0300

    h

--- src/client/kotlin/net/legitimoose/bot/LegitimooseBotClient.kt
+++ src/client/kotlin/net/legitimoose/bot/LegitimooseBotClient.kt
@@ -1,5 +1,10 @@
 package net.legitimoose.bot
 
+import java.util.concurrent.TimeUnit
+import java.util.regex.Pattern
+import kotlin.concurrent.thread
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback
 import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents
 import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents
 import net.legitimoose.bot.LegitimooseBot.config
@@ -7,16 +12,13 @@ import net.legitimoose.bot.LegitimooseBot.logger
 import net.legitimoose.bot.discord.DiscordBot
 import net.legitimoose.bot.discord.DiscordWebhook
 import net.minecraft.client.Minecraft
-import net.minecraft.client.gui.screens.TitleScreen
-import net.minecraft.client.gui.screens.multiplayer.JoinMultiplayerScreen
 import net.minecraft.client.gui.screens.ConnectScreen
 import net.minecraft.client.gui.screens.Screen
+import net.minecraft.client.gui.screens.TitleScreen
+import net.minecraft.client.gui.screens.multiplayer.JoinMultiplayerScreen
 import net.minecraft.client.multiplayer.ServerData
 import net.minecraft.client.multiplayer.resolver.ServerAddress
 import net.minecraft.network.chat.Component
-import java.util.concurrent.TimeUnit
-import java.util.regex.Pattern
-import kotlin.concurrent.thread
 
 object LegitimooseBotClient {
     private val joinPattern: Pattern = Pattern.compile("^\\[\\+]\\s*(?:[^|]+\\|\\s*)?(\\S+)")
@@ -24,18 +26,27 @@ object LegitimooseBotClient {
     private val msgPattern: Pattern = Pattern.compile("\\[(.*) -> me\\] @(.*) (.*)")
 
     fun init() {
+        ClientCommandRegistrationCallback.EVENT.register { dispatcher, _ ->
+            dispatcher.register(
+                    ClientCommandManager.literal("scrape").executes {
+                        Scraper.scrape()
+                        1
+                    }
+            )
+        }
+
         thread { DiscordBot.runBot() }
 
         ScreenEvents.AFTER_INIT.register { _: Minecraft, screen: Screen, _: Int, _: Int ->
             if (screen is TitleScreen) {
                 val info = ServerData("Server", "legitimoose.com", ServerData.Type.OTHER)
-                    ConnectScreen.startConnecting(
-                    JoinMultiplayerScreen(null),
-                    Minecraft.getInstance(),
-                    ServerAddress.parseString("legitimoose.com"),
-                    info,
-                    false,
-                    null
+                ConnectScreen.startConnecting(
+                        JoinMultiplayerScreen(null),
+                        Minecraft.getInstance(),
+                        ServerAddress.parseString("legitimoose.com"),
+                        info,
+                        false,
+                        null
                 )
             }
         }
@@ -50,8 +61,7 @@ object LegitimooseBotClient {
             while (true) {
                 try {
                     Scraper.scrape()
-                } catch (_: Exception) {
-                }
+                } catch (_: Exception) {}
                 try {
                     TimeUnit.MINUTES.sleep(config.waitMinutesBetweenScrapes)
                 } catch (e: InterruptedException) {
@@ -69,9 +79,24 @@ object LegitimooseBotClient {
 
             while (true) {
                 try {
-                    Minecraft.getInstance().player?.connection?.sendCommand("lc <br><red>I am a bot that syncs lobby chat to a community Discord")
-                    Minecraft.getInstance().player?.connection?.sendCommand("lc <br><red>If you wish to not have your messages sent to discord, prefix your messages with <u>::</u>")
-                    Minecraft.getInstance().player?.connection?.sendCommand("lc You can check out the API at <bold>https://legitimoose.net/api</bold>")
+                    Minecraft.getInstance()
+                            .player
+                            ?.connection
+                            ?.sendCommand(
+                                    "lc <br><red>I am a bot that syncs lobby chat to a community Discord"
+                            )
+                    Minecraft.getInstance()
+                            .player
+                            ?.connection
+                            ?.sendCommand(
+                                    "lc <br><red>If you wish to not have your messages sent to discord, prefix your messages with <u>::</u>"
+                            )
+                    Minecraft.getInstance()
+                            .player
+                            ?.connection
+                            ?.sendCommand(
+                                    "lc You can check out the API at <bold>https://legitimoose.net/api</bold>"
+                            )
                     TimeUnit.MINUTES.sleep(20)
                 } catch (e: InterruptedException) {
                     logger.warn(e.message)
@@ -102,11 +127,14 @@ object LegitimooseBotClient {
                     val username1 = msgMatcher.group(1)
                     val username2 = msgMatcher.group(2)
                     val msg1 = msgMatcher.group(3)
-                    val member = DiscordBot.jda.getGuildById(1311574348989071440L)!!
-                        .findMembers { s -> s.user.name == username2 }.get()[0]
-                    member.user.openPrivateChannel()
-                        .flatMap { channel -> channel.sendMessage("$username1: $msg1") }
-                        .queue()
+                    val member =
+                            DiscordBot.jda.getGuildById(1311574348989071440L)!!
+                                    .findMembers { s -> s.user.name == username2 }
+                                    .get()[0]
+                    member.user
+                            .openPrivateChannel()
+                            .flatMap { channel -> channel.sendMessage("$username1: $msg1") }
+                            .queue()
                     return@thread
                 }
 
