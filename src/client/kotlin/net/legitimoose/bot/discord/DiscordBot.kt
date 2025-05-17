623f02af8d08c149dd700a15742f05bace360e88
Omri H <76548146+omrih4@users.noreply.github.com>
2025-05-17 21:30:31 +0300

    attachment

--- src/client/kotlin/net/legitimoose/bot/discord/DiscordBot.kt
+++ src/client/kotlin/net/legitimoose/bot/discord/DiscordBot.kt
@@ -1,5 +1,6 @@
 package net.legitimoose.bot.discord
 
+import java.util.concurrent.TimeUnit
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
@@ -17,25 +18,50 @@ import net.minecraft.client.Minecraft
 import net.minecraft.client.gui.screens.ChatScreen
 import net.minecraft.client.multiplayer.PlayerInfo
 import net.minecraft.network.chat.Component
-import java.util.concurrent.TimeUnit
 
 class DiscordBot : ListenerAdapter() {
     companion object {
         lateinit var jda: JDA
         fun runBot() {
-            jda = JDABuilder.createDefault(config.discordToken)
-                .enableIntents(GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MEMBERS).build()
+            jda =
+                    JDABuilder.createDefault(config.discordToken)
+                            .enableIntents(
+                                    GatewayIntent.MESSAGE_CONTENT,
+                                    GatewayIntent.GUILD_MEMBERS
+                            )
+                            .build()
 
             jda.addEventListener(DiscordBot())
-            jda.updateCommands().addCommands(
-                Commands.slash("list", "List online players in the server")
-                    .addOption(OptionType.BOOLEAN, "lobby", "True if you only want to see online players in the lobby"),
-                Commands.slash("find", "Find which world a player is in")
-                    .addOption(OptionType.STRING, "player", "The username of the player you want to find", true),
-                Commands.slash("msg", "Message an ingame player")
-                    .addOption(OptionType.STRING, "player", "The username of the player you want to message", true)
-                    .addOption(OptionType.STRING, "message", "The message you want to send", true)
-            ).queue()
+            jda.updateCommands()
+                    .addCommands(
+                            Commands.slash("list", "List online players in the server")
+                                    .addOption(
+                                            OptionType.BOOLEAN,
+                                            "lobby",
+                                            "True if you only want to see online players in the lobby"
+                                    ),
+                            Commands.slash("find", "Find which world a player is in")
+                                    .addOption(
+                                            OptionType.STRING,
+                                            "player",
+                                            "The username of the player you want to find",
+                                            true
+                                    ),
+                            Commands.slash("msg", "Message an ingame player")
+                                    .addOption(
+                                            OptionType.STRING,
+                                            "player",
+                                            "The username of the player you want to message",
+                                            true
+                                    )
+                                    .addOption(
+                                            OptionType.STRING,
+                                            "message",
+                                            "The message you want to send",
+                                            true
+                                    )
+                    )
+                    .queue()
         }
     }
 
@@ -43,7 +69,7 @@ class DiscordBot : ListenerAdapter() {
         if (event.name == "list") {
             if (event.getOption("lobby") != null && event.getOption("lobby")!!.asBoolean) {
                 val playerList: MutableCollection<PlayerInfo>? =
-                    Minecraft.getInstance().connection?.onlinePlayers
+                        Minecraft.getInstance().connection?.onlinePlayers
                 val players = StringBuilder()
                 for (player in playerList!!) {
                     players.append(player.tabListDisplayName!!.string).append('\n')
@@ -51,7 +77,9 @@ class DiscordBot : ListenerAdapter() {
                 event.reply(players.toString()).queue()
             } else {
                 event.deferReply().queue()
-                Minecraft.getInstance().execute { Minecraft.getInstance().setScreen(ChatScreen("/find ")) }
+                Minecraft.getInstance().execute {
+                    Minecraft.getInstance().setScreen(ChatScreen("/find "))
+                }
 
                 try {
                     TimeUnit.SECONDS.sleep(1)
@@ -61,7 +89,8 @@ class DiscordBot : ListenerAdapter() {
 
                 if (Minecraft.getInstance().screen is ChatScreen) {
                     val commandSuggestions =
-                        (Minecraft.getInstance().screen as ChatScreenAccessor).commandSuggestions
+                            (Minecraft.getInstance().screen as ChatScreenAccessor)
+                                    .commandSuggestions
                     commandSuggestions.showSuggestions(false)
 
                     try {
@@ -71,13 +100,15 @@ class DiscordBot : ListenerAdapter() {
                     }
 
                     val suggestions = StringBuilder()
-                    for (suggestion in (commandSuggestions as CommandSuggestionsAccessor).getPendingSuggestions()
-                        .get().list) {
+                    for (suggestion in
+                            (commandSuggestions as CommandSuggestionsAccessor)
+                                    .getPendingSuggestions()
+                                    .get()
+                                    .list) {
                         suggestions.append(suggestion.text + '\n')
                     }
 
                     event.hook.sendMessage(suggestions.toString()).queue()
-
                 } else {
                     event.hook.sendMessage("it didn't work :shrug:").queue()
                 }
@@ -88,9 +119,10 @@ class DiscordBot : ListenerAdapter() {
                 event.reply("player name too long, sorry!").setEphemeral(true).queue()
                 return
             }
-            Minecraft.getInstance().player?.connection?.sendCommand(
-                "find " + player.replace("§", "?")
-            )
+            Minecraft.getInstance()
+                    .player
+                    ?.connection
+                    ?.sendCommand("find " + player.replace("§", "?"))
             val bool: BooleanArray = booleanArrayOf(true)
             ClientReceiveMessageEvents.GAME.register { message: Component, _: Boolean ->
                 if (!bool[0]) return@register
@@ -101,28 +133,37 @@ class DiscordBot : ListenerAdapter() {
             val message: String = event.getOption("message")!!.asString
             val player: String = event.getOption("player")!!.asString
             if ((message.length + player.length) >= 200) {
-                event.reply("Failed to send, message and/or player name too long!").setEphemeral(true).queue()
+                event.reply("Failed to send, message and/or player name too long!")
+                        .setEphemeral(true)
+                        .queue()
                 return
             }
-            Minecraft.getInstance().player?.connection?.sendCommand(
-                "msg " + player.replace("§", "?") + " [ᴅɪsᴄᴏʀᴅ] " + event.member
-                !!.effectiveName + ": " + message.replace("\n", "<br>").replace("§", "?")
-            )
-            event.reply(
-                "Sent `" + message.trim() + "` to " + event.getOption("player")
-                !!.asString
-            ).setEphemeral(true).queue()
+            Minecraft.getInstance()
+                    .player
+                    ?.connection
+                    ?.sendCommand(
+                            "msg " +
+                                    player.replace("§", "?") +
+                                    " [ᴅɪsᴄᴏʀᴅ] " +
+                                    event.member!!.effectiveName +
+                                    ": " +
+                                    message.replace("\n", "<br>").replace("§", "?")
+                    )
+            event.reply("Sent `" + message.trim() + "` to " + event.getOption("player")!!.asString)
+                    .setEphemeral(true)
+                    .queue()
         }
     }
 
     override fun onMessageReceived(event: MessageReceivedEvent) {
         if (event.isWebhookMessage || event.author.isBot) return
         val discordNick = event.member!!.effectiveName.replace("§", "?")
-        val message =
-            "<br><blue><b>ᴅɪsᴄᴏʀᴅ</b></blue> <yellow>$discordNick</yellow><dark_gray>:</dark_gray> " + event.message.contentStripped.replace(
-                "\n",
-                "<br>"
-            ).replace("§", "?")
+        var message =
+                "<br><blue><b>ᴅɪsᴄᴏʀᴅ</b></blue> <yellow>$discordNick</yellow><dark_gray>:</dark_gray> " +
+                        event.message.contentStripped.replace("\n", "<br>").replace("§", "?")
+        if (event.message.attachments.size != 0) {
+            message += " <blue>[Attachment Included]</blue>"
+        }
         if (message.length >= 200) return
         if (event.channel.id == config.channelId) {
             Minecraft.getInstance().player?.connection?.sendCommand("lc $message")
