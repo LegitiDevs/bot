0f94b4b5e47e4ba879161be6ad447daa196ed6f6
Omri H <omrih.e@protonmail.com>
2026-02-19 16:41:25 +0200

    chore: update nix shell with direnv

--- shell.nix
+++ shell.nix
@@ -3,11 +3,11 @@ let
   runtimeLibs = with pkgs; lib.makeLibraryPath [
     glfw
     libGL
-    xorg.libX11
-    xorg.libXcursor
-    xorg.libXext
-    xorg.libXrandr
-    xorg.libXxf86vm
+    libX11
+    libXcursor
+    libXext
+    libXrandr
+    libXxf86vm
     libglvnd
   ];
 in pkgs.mkShell {
