8f6a56d93e0bb03720eef82209246bec6a45fe64
Omri H <omrih.e@protonmail.com>
2026-07-30 02:30:39 +0300

    brr shaw

--- src/client/java/net/legitimoose/bot/chat/command/HelpCommand.java
+++ src/client/java/net/legitimoose/bot/chat/command/HelpCommand.java
@@ -6,8 +6,8 @@ import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 
 public class HelpCommand {
     private static final String helpMessage = "Commands:<br>!block <user> - Block <user> from sending you messages<br>!unblock <user> - Unblock <user><br>!streak - View your join streak<br>!streak <on|off> - Enable or disable streak notifications";
-    private static final String helpMessage2 = "!streak <username> - Get streak for a player<br>!streak lb|leaderboard - get streak leaderboard";
-    private static final String helpMessage3 = "!bot goto <\"x y z\"> - pathfind to coordinates (quotes required)<br>!bot follow <player> - pathfind/follow player<br>!bot spawn - run /spawn<br>!bot cancel - cancel pathfinding actions";
+    private static final String helpMessage2 = "<br>!streak <username> - Get streak for a player<br>!streak lb|leaderboard - get streak leaderboard";
+    private static final String helpMessage3 = "<br>!bot goto <\"x y z\"> - pathfind to coordinates (quotes required)<br>!bot follow <player> - pathfind/follow player<br>!bot spawn - run /spawn<br>!bot cancel - cancel pathfinding actions";
 
     public static void register(CommandDispatcher<CommandSource> dispatcher) {
         dispatcher.register(LiteralArgumentBuilder.<CommandSource>literal("help")
