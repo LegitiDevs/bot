c8763472e1d8618c47a7e7214b06585006354706
Sahil Chopra <choprasahil.sc@gmail.com>
2026-09-05 17:08:55 -0700

    feat: bot info command

--- src/client/java/net/legitimoose/bot/chat/command/BotCommands.java
+++ src/client/java/net/legitimoose/bot/chat/command/BotCommands.java
@@ -2,6 +2,8 @@ package net.legitimoose.bot.chat.command;
 
 import baritone.api.BaritoneAPI;
 import baritone.api.pathing.goals.GoalBlock;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
@@ -12,7 +14,14 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.player.Player;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class BotCommands {
+    private static final HttpClient client = HttpClient.newHttpClient();
+
     public static void register(CommandDispatcher<CommandSource> dispatcher) {
         dispatcher.register(
                 LiteralArgumentBuilder.<CommandSource>literal("bot")
@@ -74,6 +83,21 @@ public class BotCommands {
                                     context.getSource().sendMessage("Teleported to spawn!");
                                     return Command.SINGLE_SUCCESS;
                                 }))
+                        )
+                        .then(
+                                LiteralArgumentBuilder.<CommandSource>literal("info").executes(context -> {
+                                    try {
+                                        HttpRequest request = HttpRequest.newBuilder().uri(new URI("https://api.legiti.dev/")).GET().build();
+                                        String response = client.send(request, HttpResponse.BodyHandlers.ofString()).body();
+                                        JsonObject info = JsonParser.parseString(response).getAsJsonObject();
+                                        int ping = Minecraft.getInstance().getConnection().getPlayerInfo(Minecraft.getInstance().player.getUUID()).getLatency();
+
+                                        context.getSource().sendMessage(String.format("Ping: %sms<br>API Version: %s<br>Bot Version: %s", ping, info.get("version").getAsString(), info.getAsJsonObject("scraper").get("version").getAsString()));
+                                    } catch (Exception e) {
+                                        throw new RuntimeException(e);
+                                    }
+                                    return Command.SINGLE_SUCCESS;
+                                })
                         )
         );
     }
