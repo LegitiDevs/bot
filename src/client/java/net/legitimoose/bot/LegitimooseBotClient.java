5f099c2e0e13e283cb271031e349cccb2d8faaf6
Omri <omrih.e@protonmail.com>
2026-09-26 03:23:41 +0300

    chore: fix formatting errors

--- src/client/java/net/legitimoose/bot/LegitimooseBotClient.java
+++ src/client/java/net/legitimoose/bot/LegitimooseBotClient.java
@@ -3,7 +3,6 @@ package net.legitimoose.bot;
 import static net.legitimoose.bot.LegitimooseBot.CONFIG;
 import static net.legitimoose.bot.LegitimooseBot.LOGGER;
 
-//import baritone.api.BaritoneAPI;
 import com.mojang.brigadier.context.CommandContext;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -131,8 +130,8 @@ public class LegitimooseBotClient implements ClientModInitializer {
     }
 
     private void setupBaritone() {
-        //BaritoneAPI.getSettings().allowBreak.value = false;
-        //BaritoneAPI.getSettings().freeLook.value = false;
+        // BaritoneAPI.getSettings().allowBreak.value = false;
+        // BaritoneAPI.getSettings().freeLook.value = false;
     }
 
     private void scheduleExit() {
